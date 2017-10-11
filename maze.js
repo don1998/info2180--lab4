53fffc7f4a27805b637b6e47653600e3f6d92f45
don1998 <doneilscotland@yahoo.com>
2017-10-11 10:30:35 -0500

    Maze can now be reset on clicking S

--- maze.js
+++ maze.js
@@ -1,13 +1,22 @@
 window.onload=myFunction;
 
 function myFunction(){
+	var winner=0;
 	var x=document.querySelectorAll(".boundary");
 	for (let i=0;i<x.length-1;i++){
 		x[i].addEventListener("mouseover",function(){
 			for(let b=0;b<x.length-1;b++){
 				x[b].classList.add("youlose");
+				winner=1;
 			}
 		})
 	}
-
+	document.getElementById("end").addEventListener("mouseover",function(){
+		if (winner===0){
+			window.alert("You win!")
+		}
+	})
+	document.getElementById("start").addEventListener("click",function(){
+		location.reload();
+	})
 }
